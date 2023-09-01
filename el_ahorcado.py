689b617674ae6d2d7a701a8ee7032d1e196359b5
“Nicolas Burgos” <burgosdanielnicolas@gmail.com>
2023-09-01 00:09:19 -0300

    funciones del ahorcado y funcion de obtener palabra

--- el_ahorcado.py
+++ el_ahorcado.py
@@ -1,3 +1,15 @@
-print("hola Mundo")
+from palabras import palabras
 
-print("hola mundo")
+
+def obtener_palabra_valida():
+    pass
+
+
+def ahorcado():
+
+
+    print("-" * 50)
+    print("¡ Bienvenido(a) al juego del Ahorcado !")
+    print("-" * 50)
+
+    palabra = obtener_palabra_valida(palabras)
---
+++ palabras.py
@@ -0,0 +1,2 @@
+palabras = ["aire", "ojos", "piel", "anteojos", "joven", "viejo", "alto", "pequeño",
+            "gordo", "delgado", "bella", "azul", "verde", "negro"]
