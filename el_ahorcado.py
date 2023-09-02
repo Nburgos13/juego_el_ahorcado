6c91569ad75f5843cd62052e998ff142c3bab57b
“Nicolas Burgos” <burgosdanielnicolas@gmail.com>
2023-09-01 21:31:14 -0300

    Logica del While falta terminar

--- el_ahorcado.py
+++ el_ahorcado.py
@@ -4,6 +4,7 @@ import string
 
 from palabras import palabras
 
+from ahorcado_diagramas import vidas_diccionario_visual
 
 
 def obtener_palabra_valida(lista_palabras):
@@ -30,3 +31,32 @@ def ahorcado():
     abecedario = set(string.ascii_uppercase)
 
     vidas = 7
+
+    while len(letras_por_adivinar) > 0 and vidas > 0:
+
+        # Letras adivinadas
+        # " ".join({"A", "B". "C"}) -> "A, B, C"
+
+        print(f"Te quedan {vidas} vidas y has usado estas letras:"
+              f"{' '.join(letras_adivinadas)} ")
+
+        # Mostrar el estado actual de la palabra
+
+        palabra_lista = [letra if letra in letras_adivinadas
+                         else "-" for letra in palabra] # list comprehension una forma de escribir las lista
+                                                        # en una sola linea
+        # Mostrar estado del ahorcado
+        print(vidas_diccionario_visual[vidas])
+        # Mostrar las letras separadas por un espacio
+        print(f"Palabra: {' '.join(palabra_lista)}")
+
+        letra_usuario = input("Escoge una letra: ").upper()
+
+        # Si la letra escogida por el usuario est+a en el
+        # abecedario y no esta en el conjunto de letras
+        # que ya se han ingresado, se añade la letra al conjunto
+        # de letras ingresadas. 
+        if letra_usuario in abecedario - letras_adivinadas:
+            letras_adivinadas.add(letra_usuario)
+
+
